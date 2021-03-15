0f1a7b3798b1a0292406cbfcc4f387f1c995fded
touchmarine <marine@gettouch.app>
2021-03-15 16:07:31 +0100

    Add renderer templates
    Use Go templates to render elements. Implement HTML templates for
    existing elements.

--- cmd/to/main.go
+++ cmd/to/main.go
@@ -7,12 +7,22 @@ import (
 	"to/internal/node"
 	"to/internal/parser"
 	"to/internal/renderer"
+	"flag"
 )
 
+var tmpl = flag.Bool("tmpl", false, "use HTML templates")
+
 func main() {
+	flag.Parse()
+
 	nodes, err := parser.Parse(os.Stdin)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if *tmpl {
+		renderer.HTML(os.Stdout, node.BlocksToNodes(nodes))
+		return
 	}
 
 	html := renderer.Render(node.BlocksToNodes(nodes)...)
