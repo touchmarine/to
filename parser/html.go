584643fcf2569eee5fa5797ed4162e604167c6d5
touchmarine <scout@touchlabs.io>
2022-04-01 14:41:29 +0200

    Save/output To document as HTML
    The resulting HTML can be converted back to the original Touch source
    code from which the HTML outputted.

--- parser/html.go
+++ parser/html.go
@@ -15,6 +15,71 @@ import (
 // pass -dbg when testing
 var dbg = flag.Bool("dbg", false, "debug ParseFromHTML")
 
+func OutHTML(els Elements, hels map[string]atom.Atom, root *node.Node) (*html.Node, error) {
+	ctr := &html.Node{
+		Type:     html.ElementNode,
+		DataAtom: atom.Body,
+		Data:     atom.Body.String(),
+	}
+	var f func(*html.Node, *node.Node)
+	f = func(tr *html.Node, n *node.Node) {
+		if a, ok := hels[n.Element]; ok {
+			m := &html.Node{
+				Type:     html.ElementNode,
+				DataAtom: a,
+				Data:     a.String(),
+			}
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				f(m, c)
+			}
+			tr.AppendChild(m)
+			return
+		}
+
+		switch n.Type {
+		case node.TypeError:
+			panic(fmt.Sprintf("encountered an error node: %v", n.String()))
+		case node.TypeContainer, node.TypeLeaf:
+			for c := n.FirstChild; c != nil; c = c.NextSibling {
+				f(tr, c)
+			}
+			return
+		case node.TypeText:
+			m := &html.Node{
+				Type: html.TextNode,
+				Data: n.Value,
+			}
+			tr.AppendChild(m)
+			return
+		}
+
+		if n.Element == "" {
+			panic(fmt.Sprintf("blank element for non-text node: %v", n))
+		}
+		var a atom.Atom
+		if n.IsBlock() {
+			a = atom.Div
+		} else {
+			a = atom.Span
+		}
+		m := &html.Node{
+			Type:     html.ElementNode,
+			DataAtom: a,
+			Data:     a.String(),
+			Attr: map2attrs(map[string]string{
+				"class": n.Element,
+			}),
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(m, c)
+		}
+		tr.AppendChild(m)
+		return
+	}
+	f(ctr, root)
+	return ctr, nil
+}
+
 func ParseFromHTML(els Elements, hels map[atom.Atom]string, src []byte) (*node.Node, error) {
 	doc, err := html.Parse(bytes.NewReader(src))
 	if err != nil {
@@ -102,8 +167,20 @@ func ParseFromHTML(els Elements, hels map[atom.Atom]string, src []byte) (*node.N
 	return ctr, nil
 }
 
+func map2attrs(m map[string]string) []html.Attribute {
+	as := make([]html.Attribute, 0, len(m))
+	for k, v := range m {
+		as = append(as, html.Attribute{
+			Namespace: "",
+			Key:       k,
+			Val:       v,
+		})
+	}
+	return as
+}
+
 func attrs2map(as []html.Attribute) map[string]string {
-	m := map[string]string{}
+	m := make(map[string]string, len(as))
 	for _, a := range as {
 		if a.Namespace == "" {
 			m[a.Key] = a.Val
--- parser/html_test.go
+++ parser/html_test.go
@@ -7,8 +7,95 @@ import (
 	"github.com/touchmarine/to/node"
 	"github.com/touchmarine/to/parser"
 	"github.com/touchmarine/to/printer"
+	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 )
+
+func TestOutHTML(t *testing.T) {
+	hels := map[string]atom.Atom{
+		"A": atom.B,
+		"B": atom.Blockquote,
+	}
+	els := parser.Elements{
+		"A": parser.Element{
+			Name:      "A",
+			Type:      node.TypeUniform,
+			Delimiter: "*",
+		},
+		"B": parser.Element{
+			Name:      "B",
+			Type:      node.TypeWalled,
+			Delimiter: ">",
+		},
+	}
+	cases := []struct {
+		name, in, out string
+		hels          map[string]atom.Atom // overrides default hels
+	}{
+		{
+			name: "a",
+			in:   "a",
+			out:  "a",
+			hels: hels,
+		},
+		{
+			name: "**a**",
+			in:   "**a**",
+			out:  "<b>a</b>",
+			hels: hels,
+		},
+		{
+			name: "**a** no hels",
+			in:   "**a**",
+			out:  `<span class="A">a</span>`,
+			hels: nil,
+		},
+		{
+			name: ">a",
+			in:   ">a",
+			out:  "<blockquote>a</blockquote>",
+			hels: hels,
+		},
+		{
+			name: ">a no hels",
+			in:   ">a",
+			out:  `<div class="B">a</div>`,
+			hels: nil,
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			p := parser.Parser{
+				Elements: els,
+				TabWidth: 8,
+			}
+			n, err := p.Parse(nil, []byte(c.in))
+			if err != nil {
+				t.Fatalf("parse To: %v", err)
+			}
+			m, err := parser.OutHTML(els, c.hels, n)
+			if err != nil {
+				t.Fatalf("out html: %v", err)
+			}
+			h := renderHTML(t, m)
+			if h != c.out {
+				t.Errorf("\ngot:\n%s\nwant:\n%s", h, c.out)
+			}
+		})
+	}
+}
+
+func renderHTML(t *testing.T, n *html.Node) string {
+	t.Helper()
+	var b strings.Builder
+	if err := html.Render(&b, n); err != nil {
+		t.Fatalf("render html: %v", err)
+	}
+	h := b.String()
+	h = strings.TrimPrefix(h, "<body>")
+	h = strings.TrimSuffix(h, "</body>")
+	return h
+}
 
 func TestParseFromHTML(t *testing.T) {
 	hels := map[atom.Atom]string{
@@ -33,7 +120,7 @@ func TestParseFromHTML(t *testing.T) {
 			to: "**a**",
 		},
 		{
-			in: `<div class="A">a</b>`,
+			in: `<span class="A">a</div>`,
 			to: "**a**",
 		},
 		{
